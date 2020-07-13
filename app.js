f1077a54835f199fd87e9b2b173f07e9fda5e64b
kostyniuk <kostyniuk7@gmail.com>
2020-07-13 11:36:25 +0300

    dress it up

--- app.js
+++ app.js
@@ -22,18 +22,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
 
 const pgPool = new pg.Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
 
 app.use(
   session({
-    // eslint-disable-next-line new-cap
     store: new pgSession({
-      pool: pgPool, // Connection pool
+      pool: pgPool,
       tableName: 'session',
     }),
     secret: process.env.FOO_COOKIE_SECRET,
