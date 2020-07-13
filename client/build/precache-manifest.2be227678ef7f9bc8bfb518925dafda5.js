996da0a49eca5dbe932600e5c084adca9e006eda
kostyniuk <kostyniuk7@gmail.com>
2020-07-13 22:25:28 +0300

    attempt 10

---
+++ client/build/precache-manifest.2be227678ef7f9bc8bfb518925dafda5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79dcbb226ad6d6016b49681a7eaf30d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b827f8bec7f016f17fcc",
+    "url": "/static/css/main.f8a0448d.chunk.css"
+  },
+  {
+    "revision": "fc03aed87d75d818f89c",
+    "url": "/static/js/2.09f1b02a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09f1b02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b827f8bec7f016f17fcc",
+    "url": "/static/js/main.59a4e59c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
