fcc49b90f20814c3af5399aca7323e21ad7fef25
kostyniuk <kostyniuk7@gmail.com>
2020-07-14 09:07:18 +0300

    header is ready

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 import '../styles/header.css';
 
+import { TextField, InputAdornment, withStyles } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
 const Header = () => {
+  const searchHandler = (e) => {
+    if (e.key === 'Enter') {
+      console.log('Enter is pressed');
+    }
+    return;
+  };
+
+  const StyledTextField = withStyles({
+    root: {
+      background: '#ebe6e7',
+      borderRadius: 3,
+      border: 0,
+      color: 'white',
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    },
+  })(TextField);
+
   return (
     <div>
       <nav className='header-nav'>
@@ -12,19 +32,45 @@ const Header = () => {
           <span className='line'></span>
         </div>
         <div class='logo'>
-        <img src={process.env.PUBLIC_URL + '/apple-icon-60x60.png'} />
-        <a className='a-header' href='/' >Mellowgram</a>
+          <a className='a-header' href='/'>
+            Mellowgram
+          </a>
         </div>
+        <ul className='nav-search'>
+          <li className='nav-item search-field'>
+            <StyledTextField
+              onKeyPress={searchHandler}
+              color="white"
+              // className={classes}
+              id='input-with-icon-textfield'
+              variant='outlined'
+              placeholder='Search'
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </li>
+        </ul>
         <ul className='nav-items'>
           <li className='nav-item'>
-            <a className='a-header' href='#'>About us</a>
+            <a className='a-header' href='#'>
+              About us
+            </a>
           </li>
           <li>|</li>
           <li className='nav-item'>
-            <a className='a-header' href='#'>Log in</a>
+            <a className='a-header' href='#'>
+              Log in
+            </a>
           </li>
           <li className='nav-item'>
-            <a className='a-header' href='#'>Sign up</a>
+            <a className='a-header' href='#'>
+              Sign up
+            </a>
           </li>
         </ul>
       </nav>
