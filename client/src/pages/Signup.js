6cc726f697eb0dda1db6d3026beaf2cadc7be981
kostyniuk <kostyniuk7@gmail.com>
2020-07-15 16:18:03 +0300

    signup page is ready

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SignupInput from '../components/SignupInput';
 import Header from '../components/Header';
+import { useHistory } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
 
 const Signup = () => {
+  const history = useHistory();
+
+  const [form, setForm] = useState({
+    email: '',
+    fullName: '',
+    username: '',
+    password: '',
+  });
+
+  const { loading, error, request } = useFetch();
+
+  console.log({ form });
+  console.log({ loading, error });
+
+  const updateStateHandler = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const submitHandler = async () => {
+    setForm({ email: '', fullName: '', username: '', password: '' });
+
+    const responce = await request('/api/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    });
+    console.log({ responce });
+
+    if (responce.success) {
+      return history.push('/login');
+    }
+
+    return
+  };
+
   return (
     <div>
       <Header />
 
-      <SignupInput />
+      <SignupInput
+        err={error}
+        form={form}
+        changeHandler={updateStateHandler}
+        submitHandler={submitHandler}
+      />
     </div>
   );
 };
