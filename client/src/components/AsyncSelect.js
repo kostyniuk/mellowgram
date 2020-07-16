04cb3c06bbddd5eb9037da91918bcbdf77fc688e
kostyniuk <kostyniuk7@gmail.com>
2020-07-16 19:00:26 +0300

    wrap up

--- client/src/components/AsyncSelect.js
+++ client/src/components/AsyncSelect.js
@@ -2,14 +2,14 @@ import React, { Component, useState } from 'react';
 
 import AsyncSelect from 'react-select/async';
 
+import useFetch from '../hooks/useFetch';
+
 const AsyncSelectCustom = ({ handler }) => {
-  
+  const { request } = useFetch();
+
   const promiseOptions = async (inputValue) => {
     return await (async () => {
-      const res = await fetch(`/api/findUser/${inputValue}`);
-      const json = await res.json();
-      console.log({ json });
-      return json;
+      return await request(`/api/findUser/${inputValue}`);
     })();
   };
 
