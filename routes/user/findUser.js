ff52fba003923c4e67131d74f0b80ee937921a40
kostyniuk <kostyniuk7@gmail.com>
2020-07-16 19:00:12 +0300

    findUser endpoint for searching users

---
+++ routes/user/findUser.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const db = require('../../config/db');
+const router = express.Router();
+
+router.get('/:part', async (req, res, next) => {
+  const { part } = req.params;
+  const query = `SELECT * FROM User_info WHERE username::text LIKE '%${part}%';`;
+  const params = [];
+  const { rows } = await db.query(query, params);
+  console.log({ rows });
+  const usernames = rows.map((obj) => obj.username);
+
+  const responce = usernames.map((username) => ({
+    value: username,
+    label: username,
+  }));
+  console.log({ responce });
+  return res.json(responce);
+});
+
+module.exports = router;
