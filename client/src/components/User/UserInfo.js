3be3ae49b61829a4daec791ace49990e9ce35116
kostyniuk <kostyniuk7@gmail.com>
2020-07-19 13:46:20 +0300

    make it fully dynamic

--- client/src/components/User/UserInfo.js
+++ client/src/components/User/UserInfo.js
@@ -3,8 +3,7 @@ import React, { useState } from 'react';
 import '..//../styles/user.css';
 import Exprerience from './Experience';
 
-const UserInfo = ({info}) => {
-
+const UserInfo = ({ info }) => {
   const experience = [
     {
       id: 1,
@@ -19,7 +18,8 @@ const UserInfo = ({info}) => {
       company: 'Spotify',
       profession: 'JS Developer',
       achievements: 'New products',
-    },{
+    },
+    {
       id: 3,
       year: 2023,
       company: 'Spotify',
@@ -65,26 +65,22 @@ const UserInfo = ({info}) => {
     }
   };
 
-  if (!info) return <div></div>
+  if (!info) return <div></div>;
 
   return (
     <div className={cardClasses} data-state={dataState}>
       <div className='card-header'>
         <div className='card-cover'></div>
-        <img
-          className='card-avatar'
-          src={info.picture}
-          alt='avatar'
-        />
+        <img className='card-avatar' src={info.picture} alt='avatar' />
         <h1 className='card-fullname'>{info.fullname}</h1>
-        <h2 className='card-jobtitle'>JS Developer</h2>
+        <h2 className='card-jobtitle'>{info.occupation}</h2>
       </div>
       <div className='card-main'>
         <div className={sectionAbout} id='about'>
           <div className='card-content'>
             <div className='card-subtitle'>ABOUT</div>
             <p className='card-desc'>
-              {info.bio || 'The user hasn\'t provided bio information'}
+              {info.bio || "The user hasn't provided bio information"}
             </p>
           </div>
           <div className='card-social'>
@@ -136,7 +132,7 @@ const UserInfo = ({info}) => {
                   <path d='M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z' />
                   <circle cx='12' cy='10' r='3' />
                 </svg>
-                Kyiv, Ukraine
+                {info.based_in}
               </div>
               <div className='card-contact card-contact-center'>
                 <svg
@@ -151,7 +147,7 @@ const UserInfo = ({info}) => {
                 >
                   <path d='M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07 19.5 19.5 0 01-6-6 19.79 19.79 0 01-3.07-8.67A2 2 0 014.11 2h3a2 2 0 012 1.72 12.84 12.84 0 00.7 2.81 2 2 0 01-.45 2.11L8.09 9.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45 12.84 12.84 0 002.81.7A2 2 0 0122 16.92z' />
                 </svg>
-                (269) 756-9809
+                {info.phone_number || 'Not provided'}
               </div>
               <div className='card-contact'>
                 <svg
@@ -166,7 +162,7 @@ const UserInfo = ({info}) => {
                   <path d='M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z' />
                   <path d='M22 6l-10 7L2 6' />
                 </svg>
-                kostyniuk7@gmail.com
+                {info.email}
               </div>
               <button className='contact-me'>WORK TOGETHER</button>
             </div>
