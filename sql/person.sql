92afe6cd155a3873377eadc60f93459c24772956
kostyniuk <kostyniuk7@gmail.com>
2020-07-19 13:45:58 +0300

    add occupation, phone_number, based_in fields

--- sql/person.sql
+++ sql/person.sql
@@ -5,6 +5,9 @@ CREATE TABLE Person (
   email VARCHAR(32) NOT NULL,
   fullName VARCHAR(64) NOT NULL,
   picture VARCHAR(100) NOT NULL,
+  occupation VARCHAR(32) NOT NULL,
+  phone_number VARCHAR(16),
+  based_in VARCHAR(64),
   number_of_posts INTEGER NOT NULL
 );
 
@@ -15,8 +18,11 @@ ALTER TABLE Person ALTER number_of_posts SET DEFAULT 0;
 ALTER TABLE Person ALTER bio SET DEFAULT ('');
 ALTER TABLE Person ALTER picture SET DEFAULT ('./public/uploads/user_default.png');
 
-ALTER TABLE person ADD CONSTRAINT ageGreater0 CHECK(age > 0); 
+ALTER TABLE Person ADD CONSTRAINT ageGreater0 CHECK(age > 0); 
 
-ALTER TABLE person ADD CONSTRAINT fk_Person_User_Id
-  FOREIGN KEY (person_id) REFERENCES user_info (user_id)
+ALTER TABLE Person ADD CONSTRAINT uniqueEmail
+  UNIQUE (email);
+
+ALTER TABLE Person ADD CONSTRAINT fk_Person_User_Id
+  FOREIGN KEY (person_id) REFERENCES User_info (user_id)
   ON DELETE CASCADE;
