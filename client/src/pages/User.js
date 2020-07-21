0c05db20a086480b51aabab260523414071bfd8f
kostyniuk <kostyniuk7@gmail.com>
2020-07-21 18:59:01 +0300

    add an action for storing an open page info

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -1,28 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 import '../styles/user.css';
 import Header from '../components/Header/Header';
 import NotFound from './NotFound';
 import UserInfo from '../components/User/UserInfo';
 import useFetch from '../hooks/useFetch';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { setCurrentPage } from '../redux/actions';
 
 const User = ({ match, authorized }) => {
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.currentPage);
+
   const { username } = match.params;
   const [userPage, setUserPage] = useState(null);
 
-  const { error, loading, request } = useFetch();
+  const { loading, request } = useFetch();
 
-  const parseInfo = async () => {
+  const parseInfo = useCallback(async () => {
     const json = await request(`/api/user/${username}`);
     setUserPage(json.info);
-  };
+    const {
+      based_in,
+      email,
+      fullname,
+      number_of_posts,
+      occupation,
+      phone_number,
+      picture,
+    } = json.info;
+    const id = json.info.person_id;
+    dispatch(
+      setCurrentPage({
+        id,
+        username,
+        based_in,
+        email,
+        fullname,
+        number_of_posts,
+        occupation,
+        phone_number,
+        picture}
+      )
+    );
+  }, [username, request, dispatch]);
 
   useEffect(() => {
     parseInfo();
-  }, [username]);
+  }, [username, parseInfo]);
 
-  console.log({ userPage, authorized, username });
+  console.log({ userPage, currentUser });
 
   if (userPage === undefined) return <NotFound />;
 
@@ -31,7 +58,7 @@ const User = ({ match, authorized }) => {
   return (
     <div>
       <div>
-        <Header authorized={authorized}/>
+        <Header/>
       </div>
       <UserInfo info={userPage} />
     </div>
--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,10 +1,15 @@
-import { AUTH_USER, NOT_AUTH_USER } from './types';
+import { AUTH_USER, NOT_AUTH_USER, SET_CURRENT_PAGE } from './types';
 
-export const authUser = ({ id, username }) => ({
+export const authUser = ({  id, username, based_in, email, fullname, number_of_posts, occupation, phone_number, picture }) => ({
   type: AUTH_USER,
-  payload: { id, username },
+  payload: {  id, username, based_in, email, fullname, number_of_posts, occupation, phone_number, picture },
 });
 
-export const notAuthUser = {
+export const notAuthUser = () => ({
   type: NOT_AUTH_USER,
-};
+});
+
+export const setCurrentPage = ({  id, username, based_in, email, fullname, number_of_posts, occupation, phone_number, picture }) => ({
+  type: SET_CURRENT_PAGE,
+  payload: {  id, username, based_in, email, fullname, number_of_posts, occupation, phone_number, picture },
+});
