5cc787a2412e315768ebb0b48b651055301134aa
kostyniuk <kostyniuk7@gmail.com>
2020-07-22 13:51:31 +0300

    add uploadProfilePicture action and type

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,15 +1,65 @@
-import { AUTH_USER, NOT_AUTH_USER, SET_CURRENT_PAGE } from './types';
+import {
+  AUTH_USER,
+  NOT_AUTH_USER,
+  SET_CURRENT_PAGE,
+  UPDATE_PROFILE_PICTURE,
+} from './types';
 
-export const authUser = ({  id, username, based_in, email, fullname, number_of_posts, occupation, phone_number, picture }) => ({
+export const authUser = ({
+  id,
+  username,
+  based_in,
+  email,
+  fullname,
+  number_of_posts,
+  occupation,
+  phone_number,
+  picture,
+}) => ({
   type: AUTH_USER,
-  payload: {  id, username, based_in, email, fullname, number_of_posts, occupation, phone_number, picture },
+  payload: {
+    id,
+    username,
+    based_in,
+    email,
+    fullname,
+    number_of_posts,
+    occupation,
+    phone_number,
+    picture,
+  },
 });
 
 export const notAuthUser = () => ({
   type: NOT_AUTH_USER,
 });
 
-export const setCurrentPage = ({  id, username, based_in, email, fullname, number_of_posts, occupation, phone_number, picture }) => ({
+export const setCurrentPage = ({
+  id,
+  username,
+  based_in,
+  email,
+  fullname,
+  number_of_posts,
+  occupation,
+  phone_number,
+  picture,
+}) => ({
   type: SET_CURRENT_PAGE,
-  payload: {  id, username, based_in, email, fullname, number_of_posts, occupation, phone_number, picture },
+  payload: {
+    id,
+    username,
+    based_in,
+    email,
+    fullname,
+    number_of_posts,
+    occupation,
+    phone_number,
+    picture,
+  },
+});
+
+export const updateProfilePage = (picture) => ({
+  type: UPDATE_PROFILE_PICTURE,
+  payload: { picture },
 });
