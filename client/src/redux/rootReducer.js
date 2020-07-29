e79afe56fa6406cbc93f87008223720f7be5b70b
kostyniuk <kostyniuk7@gmail.com>
2020-07-29 15:02:03 +0300

    add likeReducer

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import loggedInUserReducer from './userReducer';
 import currentPageReducer from './currentPageReducer';
 import postReducer from './postReducer';
+import likeReducer from './likeReducer';
 
 const rootReducer = combineReducers({
   loggedInUser: loggedInUserReducer,
   currentPage: currentPageReducer,
   posts: postReducer,
+  likes: likeReducer,
 });
 
 export default rootReducer;
