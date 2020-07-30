9bb61620424f1d11f8b9772b2370eeaebf5863bd
kostyniuk <kostyniuk7@gmail.com>
2020-07-30 15:49:52 +0300

    initial

---
+++ client/src/components/User/FollowingBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../../styles/followingBar.css'
+
+function FollowingBar() {
+  return (
+    <div className='USER_INFO__followingBar'>
+      <h1>FollowingBar</h1>
+    </div>
+  )
+}
+
+export default FollowingBar
---
+++ client/src/components/User/PicturesBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { motion } from 'framer-motion';
+
+import '../../styles/picturesBar.css';
+
+function PicturesBar({ setSelectedImg }) {
+  const pictures = [
+    { id: 1, url: '/api/public/uploads/user_kostyniuk.jpg' },
+    { id: 1, url: '/api/public/uploads/user_dloading.jpg' },
+    { id: 1, url: '/api/public/uploads/N6NCsEnf_6U9RvrfYNXpb.jpg' },
+    { id: 1, url: '/api/public/uploads/user_tsunamipapi.jpg' },
+  ];
+
+  return (
+    <div className='USER_INFO__picturesBar'>
+      <h3 className='USER_INFO__picturesBar__title'>My pictures</h3>
+      <div className='img-grid'>
+        {pictures &&
+          pictures.map((doc) => (
+            <motion.div
+              className='img-wrap'
+              key={doc.id}
+              layout
+              onClick={() => setSelectedImg(doc.url)}
+            >
+              <motion.img
+                src={doc.url}
+                alt='uploaded pic'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1 }}
+              />
+            </motion.div>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default PicturesBar;
