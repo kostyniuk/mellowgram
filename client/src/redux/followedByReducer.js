0a15a1b63afecc5d06d0556809352a84ddc49f01
kostyniuk <kostyniuk7@gmail.com>
2020-08-03 20:07:28 +0300

    add actions

--- client/src/redux/followedByReducer.js
+++ client/src/redux/followedByReducer.js
@@ -1,4 +1,4 @@
-import { SET_FOLLOWED_BY } from './types';
+import { SET_FOLLOWED_BY, ADD_FOLLOW, DELETE_FOLLOW } from './types';
 
 const initialState = {
   user: null,
@@ -7,9 +7,29 @@ const initialState = {
 const followedByReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_FOLLOWED_BY: {
+
       const { users, user } = action.payload;
 
       return { ...state, users, user };
+    }
+
+
+    case ADD_FOLLOW: {
+      const { id, picture, username } = action.payload.producer;
+
+      let { users } = state;
+      users.push({ person_id: id, picture, username });
+
+      return { ...state, users };
+    }
+
+    case DELETE_FOLLOW: {
+      const { id } = action.payload.producer;
+      let { users } = state;
+
+      users = users.filter((user) => user.person_id !== id);
+
+      return { ...state, users };
     }
 
     default: {
