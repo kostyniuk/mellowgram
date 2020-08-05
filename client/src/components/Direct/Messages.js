81bc932129be1187eaf48d2b4cbdd6ab1648d32d
kostyniuk <kostyniuk7@gmail.com>
2020-08-05 20:39:48 +0300

    top and bottom layers ready

---
+++ client/src/components/Direct/Messages.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import '../../styles/messages.css';
+import PostInput from '../User/Post/PostInput';
+
+const Messages = () => {
+  const data = {
+    username: 'dloading',
+    picture: 'http://localhost:3000/api/public/uploads/user_dloading.jpg',
+    messages: [
+      { id: 1, from: 'dloading', text: 'Hi there', date: '3:22 am' },
+      {
+        id: 2,
+        from: 'kostyniuk',
+        text: 'Hi dasadsdsadasxzczxc',
+        date: '13:22 am',
+      },
+      { id: 3, from: 'dloading', text: 'Hi ', date: '23:22' },
+      { id: 4, from: 'dloading', text: 'daadsqweqwewq', date: '12.03' },
+      { id: 5, from: 'kostyniuk', text: 'Hi', date: '2019' },
+      { id: 6, from: 'kostyniuk', text: 'Hi there', date: 'Wed' },
+    ],
+  };
+
+  return (
+    <div className='MESSAGES__container'>
+      <div className='MESSAGES__title'>
+        <i className='fa fa-caret-left MESSAGES_back_button'></i>
+        <h4>{data.username}</h4>
+        <div className='MESSAGES_title_picture'>
+          <img src={data.picture}></img>
+        </div>
+      </div>
+      <div className='MESSAGES__body'>Body</div>
+      <div className='MESSAGES__send'>
+        <textarea
+          name='message'
+          autoFocus
+          rows='1'
+          className='form-input MESSAGES_INPUT'
+        ></textarea>
+        <i class="fa fa-paper-plane"></i>
+      </div>
+    </div>
+  );
+};
+
+export default Messages;
