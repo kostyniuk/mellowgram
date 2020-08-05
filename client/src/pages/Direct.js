bf3625d2d08bad6ca8c598146b9f7879bc07c0c7
kostyniuk <kostyniuk7@gmail.com>
2020-08-05 20:39:59 +0300

    add Messages

--- client/src/pages/Direct.js
+++ client/src/pages/Direct.js
@@ -4,6 +4,7 @@ import Header from '../components/Header/Header';
 
 import '../styles/direct.css';
 import Chats from '../components/Direct/Chats';
+import Messages from '../components/Direct/Messages';
 
 const Direct = () => {
   return (
@@ -13,7 +14,9 @@ const Direct = () => {
         <div className='DIRECT__left_side'>
           <Chats />
         </div>
-        <div className='DIRECT__right_side'></div>
+        <div className='DIRECT__right_side'>
+          <Messages />
+        </div>
       </div>
     </div>
   );
