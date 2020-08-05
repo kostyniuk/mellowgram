540d249cc3fa9802e39026eeb321a5ee326cdd25
kostyniuk <kostyniuk7@gmail.com>
2020-08-05 21:07:10 +0300

    component is implemented

---
+++ client/src/components/Direct/Message.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Message = ({ context, isOwner, date }) => {
+  let className = 'MESSAGES__chat-msg ';
+
+  if (isOwner) className += 'MESSAGES__owner';
+
+  return (
+    <div className={className}>
+      <div className='MESSAGES__chat-msg-content'>
+        <div class='MESSAGES__chat-msg-text'>{context}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
