b55edf8d1c87f25c83e4ea04f8599bd3a3a546bd
kostyniuk <kostyniuk7@gmail.com>
2020-08-07 14:26:05 +0300

    remove mock data

--- client/src/pages/Direct.js
+++ client/src/pages/Direct.js
@@ -1,131 +1,32 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import equal from 'deep-equal';
-import { v4 as uuidv4 } from 'uuid';
 import Header from '../components/Header/Header';
 
 import '../styles/direct.css';
 import Chats from '../components/Direct/Chats';
 import Messages from '../components/Direct/Messages';
-import { compareSync } from 'bcryptjs';
+import { getChats, getMessages } from '../redux/actions';
 
-const chats1 = [
-  {
-    id: 1,
-    name: 'steph',
-    picture:
-      'http://localhost:3000/api/public/uploads/N6NCsEnf_6U9RvrfYNXpb.jpg',
-    latestMessage: {
-      id: 100,
-      text: 'Hi kostyniuk',
-      sendAt: 'Fri',
-    },
-    unreadMessagesCount: 10,
-  },
-  {
-    id: 2,
-    name: 'dloading',
-    picture: 'http://localhost:3000/api/public/uploads/user_dloading.jpg',
-    latestMessage: {
-      id: 101,
-      text: 'lol 😂😂😂😂😂😂 sadsdada ssssssssdas sssss  ssssssss',
-      sendAt: '14:14 pm',
-    },
-    unreadMessagesCount: 0,
-  },
-  {
-    id: 3,
-    name: 'kostyniuk',
-    picture: 'http://localhost:3000/api/public/uploads/user_kostyniuk.jpg',
-    latestMessage: {
-      id: 102,
-      text: 'ngl you made it 😛😛😛ads adsdasdadsasda',
-      sendAt: '17.06',
-    },
-    unreadMessagesCount: 1,
-  },
-  {
-    id: 4,
-    name: 'tsunamipapi',
-    picture:
-      'http://localhost:3000/api/public/uploads/uxWTUo5M62aoDxkEO3hB7.jpeg',
-    latestMessage: {
-      id: 102,
-      text: 'ngl you made it 😛😛😛ads adsdasdadsasda',
-      sendAt: '17.06',
-    },
-    unreadMessagesCount: 1,
-  },
-];
 
-const messages1 = [
-  {
-    user_id: 12,
-    username: 'dloading',
-    picture: 'http://localhost:3000/api/public/uploads/user_dloading.jpg',
-    messages: [
-      { id: 1, from: 'dloading', text: 'STEPH MESSAGE 1', date: '3:22 am' },
-      {
-        id: 2,
-        from: 'dloading',
-        text: 'DLOADING MESSAGE 2',
-        date: '13:22 am',
-      },
-    ],
-  },
-  {
-    user_id: 8,
-    username: 'steph',
-    picture:
-      'http://localhost:3000/api/public/uploads/N6NCsEnf_6U9RvrfYNXpb.jpg',
-    messages: [
-      { id: 1, from: 'steph', text: 'STEPH MESSAGE 1', date: '3:22 am' },
-      {
-        id: 2,
-        from: 'kostyniuk',
-        text: 'STEPH MESSAGE 1',
-        date: '13:22 am',
-      },
-    ],
-  },
-  {
-    user_id: 14,
-    username: 'tsunamipapi',
-    picture:
-      'http://localhost:3000/api/public/uploads/uxWTUo5M62aoDxkEO3hB7.jpeg',
-    messages: [
-      { id: 1, from: 'dloading', text: 'KELLY MESSAGE 1', date: '3:22 am' },
-      {
-        id: 2,
-        from: 'kostyniuk',
-        text: 'KELlY MESSAGE 1',
-        date: '13:22 am',
-      },
-    ],
-  },
-  {
-    user_id: 7,
-    username: 'kostyniuk',
-    picture: 'http://localhost:3000/api/public/uploads/user_kostyniuk.jpg',
-    messages: [
-      { id: 1, from: 'dloading', text: 'STEPH MESSAGE 1', date: '3:22 am' },
-      {
-        id: 2,
-        from: 'kostyniuk',
-        text: 'DLOADING MESSAGE 2',
-        date: '13:22 am',
-      },
-    ],
-  },
-];
-
-const uuid = null;
-
-const ws = new WebSocket(`ws://localhost:5000`);
-// const ws = new WebSocket(`wss://mellowgram.herokuapp.com/`);
+// const ws = new WebSocket(`ws://localhost:5000`);
+const ws = new WebSocket(`wss://mellowgram.herokuapp.com/`);
 const Direct = () => {
+  const dispatch = useDispatch();
+
   const [openDialog, setOpenDialog] = useState(null);
-  const [chats, setChats] = useState(null);
+  const chats = Object.values(
+    useSelector(
+      (state) => state.chats,
+      (prev, curr) => equal(prev, curr)
+    )
+  );
+  const messages = Object.values(
+    useSelector(
+      (state) => state.messages,
+      (prev, curr) => equal(prev, curr)
+    )
+  );
 
   const loggedInUser = useSelector(
     (state) => state.loggedInUser,
@@ -142,7 +43,17 @@ const Direct = () => {
       switch (action) {
         case 'GET_CHATS':
           console.log({ message });
-          setChats(message.payload.rooms);
+          dispatch(
+            getChats({
+              chats: message.payload.rooms,
+            })
+          );
+          dispatch(
+            getMessages({
+              messages: message.payload.messages,
+              chats: message.payload.rooms,
+            })
+          );
           break;
 
         default:
@@ -150,14 +61,16 @@ const Direct = () => {
       }
     };
 
-    ws.send(JSON.stringify({ action: 'GET_CHATS', id: loggedInUser.id }));
+    if (!chats.length) {
+      ws.send(JSON.stringify({ action: 'GET_CHATS', id: loggedInUser.id }));
+    }
 
     ws.onclose = () => {
       console.log('Connection closed');
     };
   }, []);
 
-  if (!chats) return <div></div>;
+  if (!chats.length || !messages.length) return <div></div>;
 
   return (
     <div className='DIRECT__page'>
@@ -174,7 +87,7 @@ const Direct = () => {
           <Messages
             data={
               openDialog
-                ? messages1.filter((msgs) => msgs.username === openDialog)[0]
+                ? messages.filter((msgs) => +msgs.room_id === +openDialog)[0]
                 : null
             }
             socket={ws}
