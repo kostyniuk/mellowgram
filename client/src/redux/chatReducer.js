d28914f1f4b83ddab5b64e0b886471034fd10c76
kostyniuk <kostyniuk7@gmail.com>
2020-08-08 11:22:32 +0300

    dynamical number of unread

--- client/src/redux/chatReducer.js
+++ client/src/redux/chatReducer.js
@@ -1,15 +1,52 @@
-import {GET_CHATS } from './types';
+import { GET_CHATS, ADD_MESSAGE } from './types';
 import { arrToObj } from '../helpers/index';
 const initialState = {};
 
 const chatReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_CHATS: {
-      const { chats } = action.payload;
+      const { chats, messages, me } = action.payload;
 
       const transformed = arrToObj(chats, 'room_id');
 
-      return { ...state, ...transformed };
+      const addNumOfUnread = (messages) => {
+        let final = { ...transformed };
+
+        for (const chatObj in final) {
+          final[chatObj] = { ...final[chatObj], unread: 0 };
+        }
+
+        messages
+          .filter((message) => message.sender_id !== me.id)
+          .filter((message) => !message.is_read)
+          .map((message) => {
+            if (Object.keys(final).includes(message.room_id.toString())) {
+              final[message.room_id].unread++;
+            }
+          });
+
+        return final;
+      };
+
+      const patched = addNumOfUnread(messages);
+
+      return { ...state, ...patched };
+    }
+
+    case ADD_MESSAGE: {
+      const { info } = action.payload;
+
+      const temp = JSON.parse(JSON.stringify(state));
+      temp[info.roomId].latestMessage = {
+        message_id: info.messageId,
+        room_id: info.roomId,
+        sender_id: info.senderId,
+        context: info.context,
+        send_at: info.date,
+        is_read: true,
+      };
+
+      return { ...state, ...temp };
     }
 
     default: {
