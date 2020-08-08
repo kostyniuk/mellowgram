0f760a520bc4a16d729e39f954fc4265907fdce6
kostyniuk <kostyniuk7@gmail.com>
2020-08-08 17:07:00 +0300

    adjust it

--- client/src/components/Direct/Message.js
+++ client/src/components/Direct/Message.js
@@ -8,14 +8,14 @@ const Message = ({ context, isOwner, date }) => {
   if (isOwner) className += 'MESSAGES__owner';
 
   return (
-    <motion.div layout className={className}>
+    <div layout className={className}>
       <div className='MESSAGES__chat-msg-content'>
         <div class='MESSAGES__chat-msg-text'>
           <div className='MESSAGES__chat-msg-text-context'>{context}</div>
           <div className='MESSAGES__chat-msg-date'>{date}</div>
         </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
