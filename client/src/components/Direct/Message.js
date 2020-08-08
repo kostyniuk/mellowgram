84fce91fb4d06ea54f79c3cf966ad4872cd9550a
kostyniuk <kostyniuk7@gmail.com>
2020-08-08 16:52:06 +0300

    add animation to messages

--- client/src/components/Direct/Message.js
+++ client/src/components/Direct/Message.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-
+import { motion } from 'framer-motion';
 
 const Message = ({ context, isOwner, date }) => {
   let className = 'MESSAGES__chat-msg ';
@@ -8,14 +8,14 @@ const Message = ({ context, isOwner, date }) => {
   if (isOwner) className += 'MESSAGES__owner';
 
   return (
-    <div className={className}>
+    <motion.div layout className={className}>
       <div className='MESSAGES__chat-msg-content'>
         <div class='MESSAGES__chat-msg-text'>
           <div className='MESSAGES__chat-msg-text-context'>{context}</div>
           <div className='MESSAGES__chat-msg-date'>{date}</div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
