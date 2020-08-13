262c8854493431b67fe9572d103f3ba3aa66f1d2
kostyniuk <kostyniuk7@gmail.com>
2020-08-13 19:03:55 +0300

    fix bug with empty chats beibg not in the bottom

--- client/src/components/Direct/Chats.js
+++ client/src/components/Direct/Chats.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 import ChatRow from './ChatRow';
 
+import { sortChatsByLatestMessage } from '../../helpers';
+
 import '../../styles/direct.css';
 
 const Chats = ({ chats, openDialog, handleChatClick, empty }) => {
+  const emptyChats = chats.filter((element) => !element.latestMessage);
+  const notEmpty = chats.filter((element) => element.latestMessage);
+
+  notEmpty.sort(sortChatsByLatestMessage);
+
+  const allChats = [...notEmpty, ...emptyChats];
+
   return (
     <div>
-      {chats.map((chat) => (
+      {allChats.map((chat) => (
         <ChatRow
           key={chat.id}
           chat_id={chat.room_id}
