4376e5b27e3993bcdcb1bb617e910469ef458d03
kostyniuk <kostyniuk7@gmail.com>
2020-08-13 18:21:26 +0300

    move here two functions from ChatRow

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -23,4 +23,31 @@ module.exports = {
     chats.reduce((prev, cur) => {
       return cur.unread + prev;
     }, 0),
+  adjustTime: (date) => {
+    if (!date) return null;
+
+    const withoutTimeZone = date
+      .split('T')[1]
+      .split('.')[0]
+      .split(':')
+      .slice(0, 2);
+
+    const myTime = withoutTimeZone.map((num, i) => {
+      if (i === 0) {
+        return (Number(num) + 6).toString();
+      }
+      return num;
+    });
+
+    return myTime.join(':');
+  },
+  prepareText: (str) => {
+    if (!str) return 'No messages yet';
+
+    if (str.length > 34) {
+      return str.toString().substring(0, 36).concat('...');
+    } else {
+      return str;
+    }
+  },
 };
