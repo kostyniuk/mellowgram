436a5641dfbfb55ac5f86884d336134a3c9b1a08
kostyniuk <kostyniuk7@gmail.com>
2020-08-13 10:46:54 +0300

    change algorithm so delete unused props

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -63,6 +63,7 @@ const User = ({ match, startMessagingHandler }) => {
       <div>
         <Header />
       </div>
+                  
       <UserInfo startMessagingHandler={startMessagingHandler}/>
       <Posts />
     </div>
