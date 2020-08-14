328e275040c025b255c27fb2d8bb777a15e60d59
kostyniuk <kostyniuk7@gmail.com>
2020-08-14 10:50:14 +0300

    implementing DateHr

--- client/src/components/Direct/Message.js
+++ client/src/components/Direct/Message.js
@@ -1,21 +1,24 @@
 import React from 'react';
 
 import { motion } from 'framer-motion';
+import DateHr from './DateHr';
 
-const Message = ({ context, isOwner, date }) => {
+const Message = ({ type, context, isOwner, date }) => {
   let className = 'MESSAGES__chat-msg ';
 
   if (isOwner) className += 'MESSAGES__owner';
 
+  if (type) return <DateHr date={date} />;
+
   return (
-    <div layout className={className}>
+    <motion.div layout className={className}>
       <div className='MESSAGES__chat-msg-content'>
         <div class='MESSAGES__chat-msg-text'>
           <div className='MESSAGES__chat-msg-text-context'>{context}</div>
           <div className='MESSAGES__chat-msg-date'>{date}</div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
