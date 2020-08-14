644205490c6246f80cc3a0e0240d6a3d3f99606c
kostyniuk <kostyniuk7@gmail.com>
2020-08-14 10:53:28 +0300

    move addTimeSeparator function here

--- client/src/helpers/msg.js
+++ client/src/helpers/msg.js
@@ -26,3 +26,91 @@ export const prepareText = (str) => {
     return str;
   }
 };
+
+export const addTimeSeparator = (messages) => {
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const reduced = messages.reduce((prev, current) => {
+    const currentMessageDate = {};
+    const previousMessageDate = {};
+
+    currentMessageDate.day = new Date(current.date).getDate();
+    currentMessageDate.month = new Date(current.date).getMonth();
+    currentMessageDate.year = new Date(current.date).getFullYear();
+
+    const previousMessage = prev[prev.length - 1];
+
+    if (prev.length === 0)
+      return [
+        ...prev,
+        {
+          type: 'separator',
+          date: `${currentMessageDate.day} ${
+            monthNames[currentMessageDate.month]
+          }`,
+        },
+        current,
+      ];
+
+    previousMessageDate.day = new Date(previousMessage.date).getDate();
+    previousMessageDate.month = new Date(previousMessage.date).getMonth();
+    previousMessageDate.year = new Date(previousMessage.date).getFullYear();
+
+    if (previousMessageDate.day !== currentMessageDate.day) {
+      return [
+        ...prev,
+        {
+          type: 'separator',
+          room_id: current.room_id,
+          date: `${currentMessageDate.day} ${
+            monthNames[currentMessageDate.month]
+          }`,
+        },
+        current,
+      ];
+    }
+
+    if (previousMessageDate.month !== currentMessageDate.month) {
+      return [
+        ...prev,
+        {
+          type: 'separator',
+          date: `${currentMessageDate.day} ${
+            monthNames[currentMessageDate.month]
+          }`,
+        },
+        current,
+      ];
+    }
+
+    if (previousMessageDate.year !== currentMessageDate.year) {
+      return [
+        ...prev,
+        {
+          type: 'separator',
+          date: `${currentMessageDate.day} ${
+            monthNames[currentMessageDate.month]
+          } ${currentMessageDate.year}`,
+        },
+        current,
+      ];
+    }
+
+    return [...prev, current];
+  }, []);
+
+  return reduced;
+};
