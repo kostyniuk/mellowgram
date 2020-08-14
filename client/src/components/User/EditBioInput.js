3abb8ca10987578245667c8a51c165175fff6dc8
kostyniuk <kostyniuk7@gmail.com>
2020-08-14 14:32:43 +0300

    intial

---
+++ client/src/components/User/EditBioInput.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import useFetch from '../../hooks/useFetch';
+
+const EditBioInput = ({ bio }) => {
+  const { request } = useFetch();
+
+  const [newBio, setNewBio] = useState(bio || '');
+
+  const submitBio = async () => {
+    const result = await request('/api/user/bio', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ bio: newBio }),
+    });
+
+    setNewBio('');
+
+    if (result.success) {
+      console.log('Bio updated');
+    }
+  };
+
+  return (
+    <div>
+      <textarea
+        value={newBio}
+        name='message'
+        autoFocus
+        id='message'
+        rows='4'
+        className='form-input'
+        onChange={(e) => setNewBio(e.target.value)}
+      ></textarea>
+      <button className='green' onClick={submitBio}>
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default EditBioInput;
