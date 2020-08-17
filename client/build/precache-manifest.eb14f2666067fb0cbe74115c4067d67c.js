12008c12411ca5f4c121ac2451ad1cfe278b9ee4
kostyniuk <kostyniuk7@gmail.com>
2020-08-17 21:40:47 +0300

    new build

---
+++ client/build/precache-manifest.eb14f2666067fb0cbe74115c4067d67c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a098f84e67bc0339e62d98ce089597",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f6024d07d0b1b909dbe",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "631d02d7c7705db94670",
+    "url": "/static/css/main.a3a174ae.chunk.css"
+  },
+  {
+    "revision": "7f6024d07d0b1b909dbe",
+    "url": "/static/js/2.392e9b01.chunk.js"
+  },
+  {
+    "revision": "f7f6df676dd777668e07d14369e57924",
+    "url": "/static/js/2.392e9b01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631d02d7c7705db94670",
+    "url": "/static/js/main.35568c07.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.161d7cf8b1aedd4c4753899d8ecdde94.js"
+  "/precache-manifest.eb14f2666067fb0cbe74115c4067d67c.js"
 );
 
 self.addEventListener('message', (event) => {
