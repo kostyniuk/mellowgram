639654a5c9ecf56b7987d7c7ef96f158233fc1d8
kostyniuk <kostyniuk7@gmail.com>
2020-08-17 20:38:59 +0300

    using newly created components from './settings

--- client/src/components/SettingsCard.js
+++ client/src/components/SettingsCard.js
@@ -4,14 +4,16 @@ import { useSelector, useDispatch } from 'react-redux';
 import { updateProfileInfo } from '../redux/actions';
 import useFetch from '../hooks/useFetch';
 
-import Table from './Table';
-import UploadButton from './UploadButton';
-
 import { deepCopy, deleteProperties } from '../helpers';
 
 import '../styles/table.css';
 import '../styles/btn.css';
-import SelectInterests from './SelectInterests';
+import Overview from './Settings/Overview';
+import EditProfile from './Settings/EditProfile';
+import EditInterests from './Settings/EditInterests';
+import ChangePassword from './Settings/ChangePassword';
+import DeleteProfile from './Settings/DeleteProfile';
+import Tabs from './Settings/Tabs';
 
 const SettingsCard = () => {
   const dispatch = useDispatch();
@@ -81,6 +83,8 @@ const SettingsCard = () => {
     setPasswordToDelete('');
   };
 
+  const sendNewActivities = async () => {};
+
   const copy1 = deepCopy(info);
 
   const adjustedOverwiew = deleteProperties(copy1, [
@@ -88,132 +92,38 @@ const SettingsCard = () => {
     'ready',
     'isAuthenticated',
     'picture',
-    'interests'
+    'interests',
   ]);
 
   return (
     <div className='settings-container'>
       <div className='left-column'>
-        <ul>
-          <li onClick={switchTab.bind(null, 0)}>
-            <i className='fa fa-user'></i>Overview
-          </li>
-          <hr class='HRsettings' />
-          <li onClick={switchTab.bind(null, 1)}>
-            <i className='fa fa-cog'></i>Edit Profile
-          </li>
-          <hr class='HRsettings' />
-          <li onClick={switchTab.bind(null, 2)}>
-            <i className='fa fa-list-alt'></i>Edit Interests
-          </li>
-          <hr class='HRsettings' />
-          <li onClick={switchTab.bind(null, 3)}>
-            <i className='fa fa-lock'></i>Change Password
-          </li>
-          <hr class='HRsettings' />
-          <li onClick={switchTab.bind(null, 4)}>
-            <i className='fa fa-minus-circle'></i>Delete profile
-          </li>
-        </ul>
+        <Tabs switchTab={switchTab} />
       </div>
       <div className='settings-tab'>
         {openTab === 0 && (
-          <div className='settings-overview'>
-            <h1>Account overview</h1>
-            <div className='settings-overview-img'>
-              <img src={info.picture} alt='avatar'></img>
-              <UploadButton />
-            </div>
-            <div className='settings-overview-table'>
-              <Table tab='overview' data={adjustedOverwiew} />
-            </div>
-          </div>
+          <Overview info={info} adjustedOverwiew={adjustedOverwiew} />
         )}
         {openTab === 1 && (
-          <form className='settings-form'>
-            <div className='settings-overview'>
-              <h1>Edit profile</h1>
-              <Table tab='edit' data={edit} handler={editHandlerOnChange} />
-              <button className='btn green' onClick={submitEdit}>
-                Submit
-              </button>
-              {error && (
-                <h2 style={{ color: 'red', marginTop: '10px' }}>
-                  Error: {error}
-                </h2>
-              )}
-              {updatedInfo && (
-                <h2 style={{ color: 'green', marginTop: '10px' }}>
-                  Information updated
-                </h2>
-              )}
-            </div>
-          </form>
+          <EditProfile
+            edit={edit}
+            editHandlerOnChange={editHandlerOnChange}
+            error={error}
+            submitEdit={submitEdit}
+            updatedInfo={updatedInfo}
+          />
         )}
         {openTab === 2 && (
-          <div className='settings_edit_interests'>
-            <div>
-              <h1>Edit interests</h1>
-              <SelectInterests />
-            </div>
-            <button className='btn green' onClick={submitEdit}>
-              Submit
-            </button>
-            {error && (
-              <h2 style={{ color: 'red', marginTop: '10px' }}>
-                Error: {error}
-              </h2>
-            )}
-            {updatedInfo && (
-              <h2 style={{ color: 'green', marginTop: '10px' }}>
-                Information updated
-              </h2>
-            )}
-          </div>
+          <EditInterests sendNewActivities={sendNewActivities} />
         )}
-        {openTab === 3 && (
-          <form onSubmit={() => console.log('Submitted')}>
-            <h1>Change password</h1>
-            <Table
-              tab='edit'
-              data={{
-                'Old Password': '',
-                'New Password': '',
-                'Confirm new password': '',
-              }}
-            />
-            <button className='btn green'>Submit</button>
-          </form>
-        )}
+        {openTab === 3 && <ChangePassword />}
         {openTab === 4 && (
-          <form>
-            <h1>Delete your profile</h1>
-            <h2>
-              Are you sure you want to delete your profile ?<br /> All your
-              matchs will be lost...
-            </h2>
-            <div className='input-group'>
-              <Table
-                tab='edit'
-                data={{
-                  Password: passwordToDelete,
-                }}
-                handler={deleteHadlerOnChange}
-              />
-              <button
-                type='button'
-                className='green'
-                onClick={deleteHandlerOnClick}
-              >
-                Submit
-              </button>
-              {wrongPassword && (
-                <h2 style={{ color: 'red', marginTop: '10px' }}>
-                  Wrong password
-                </h2>
-              )}
-            </div>
-          </form>
+          <DeleteProfile
+            deleteHadlerOnChange={deleteHadlerOnChange}
+            deleteHandlerOnClick={deleteHandlerOnClick}
+            wrongPassword={wrongPassword}
+            passwordToDelete={passwordToDelete}
+          />
         )}
       </div>
     </div>
