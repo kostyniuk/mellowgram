52d8df9b2e0128ac30d0f26cb550f47ab2935aa5
kostyniuk <kostyniuk7@gmail.com>
2020-08-19 10:01:08 +0300

    implement /post/home

--- routes/post.js
+++ routes/post.js
@@ -5,10 +5,55 @@ const router = express.Router({ mergeParams: true });
 const isAvailable = require('../lib/isOwnPage');
 const db = require('../config/db');
 
+const fetchEssentInfo = require('../lib/fetchUserEssentialInfo');
 const transformCreationTime = require('../lib/transformCreationTime');
 const formatTime = require('../lib/formatTime');
+const { formParams } = require('../lib/sqlUtils');
 
-// posts handling
+router.get('/home', async (req, res, next) => {
+  try {
+    const { username } = req.user;
+
+    let { limit, offset } = req.query;
+
+    limit = limit || 100;
+    offset = offset || 0;
+
+    const query = `SELECT followed_id FROM Follow WHERE following_id = (SELECT user_id FROM user_info WHERE username=$1);`;
+    const params = [username];
+    const { rows } = await db.query(query, params);
+
+    if (rows.length) {
+      const userIds = rows.map((obj) => Object.values(obj)).flat();
+
+      const addParameters = formParams(userIds.length, 2);
+
+      const queryPosts = `SELECT post_id, creator_id, caption, created_at, number_of_likes FROM Post
+    WHERE creator_id in (${addParameters})
+    ORDER BY post_id DESC OFFSET $1 LIMIT $2;`;
+      const parametrsPosts = [offset, limit, ...userIds];
+
+      const posts = await db.query(queryPosts, parametrsPosts);
+      const creatorsInfo = await fetchEssentInfo(userIds);
+
+      const patchedPosts = posts.rows.map((post) => {
+        const { creator_id } = post;
+        const creator = creatorsInfo.filter(
+          (creator) => creator.person_id === creator_id
+        )[0];
+        return { ...post, creatorInfo: creator };
+      });
+
+      return res.status(200).json({ success: true, data: patchedPosts });
+    }
+
+    return res.json({ success: true });
+  } catch (e) {
+    console.error(e);
+    res.json({ success: false, posts: null });
+  }
+});
+
 router.get('/:username', async (req, res, next) => {
   try {
     const { username } = req.params;
