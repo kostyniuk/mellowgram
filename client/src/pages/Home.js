89bb0af1d6e7dba9094e46f0dd51298321f294e6
kostyniuk <kostyniuk7@gmail.com>
2020-08-19 12:53:50 +0300

    add header

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from '../components/Header/Header';
+import Feed from '../components/Home/Feed';
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <Header />
+      <Feed />
     </div>
   );
 };
