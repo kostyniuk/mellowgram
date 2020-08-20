96f8ae83a5a87dc83ecbd891dba6cdfacd4f09ee
kostyniuk <kostyniuk7@gmail.com>
2020-08-20 10:15:08 +0300

    fix bug with selectedLikes

--- client/src/components/Home/Feed.js
+++ client/src/components/Home/Feed.js
@@ -7,12 +7,16 @@ import { sleep } from '../../helpers/index';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import Post from '../User/Post/Post';
 
+import '../../styles/home.css';
+import LikesModal from '../User/LikesModal';
+
 const Feed = () => {
   const { request } = useFetch();
   const dispatch = useDispatch();
 
   const posts = Object.values(useSelector((state) => state.homePosts)).sort();
   const [hasMore, setHasMore] = useState(true);
+  const [selectedLikes, setSelectedLikes] = useState(null);
 
   console.log({ posts });
 
@@ -66,35 +70,52 @@ const Feed = () => {
   }, [loadIntitialPosts]);
 
   return (
-    <div>
-      <h1>Home</h1>
-      {posts.length && (
-        <InfiniteScroll
-          dataLength={posts.length}
-          next={fetchMoreData}
-          hasMore={hasMore}
-          loader={<h4>Loading...</h4>}
-        >
-          {posts
-            .reverse()
-            .slice(1)
-            .map((post) => {
-              return (
-                <Post
-                  key={post.post_id}
-                  id={post.post_id}
-                  picture={post.creatorInfo.picture}
-                  username={post.creatorInfo.username}
-                  text={post.caption}
-                  numberOfLikes={post.number_of_likes}
-                  postedAt={post.created_at}
-                  likes={post.likes}
-                  type='feed'
-                  // setSelectedLikes={setSelectedLikes}
-                />
-              );
-            })}
-        </InfiniteScroll>
+    <div className='FEED_CONTAINER'>
+      <div className='FEED_SIDE'>
+        <h3>Recomendations</h3>
+        <hr></hr>
+      </div>
+      <div className='POSTS__container'>
+        {posts.length ? (
+          <InfiniteScroll
+            dataLength={posts.length}
+            next={fetchMoreData}
+            hasMore={hasMore}
+            loader={<h4>Loading...</h4>}
+          >
+            {posts
+              .reverse()
+              .slice(1)
+              .map((post) => {
+                return (
+                  <Post
+                    key={post.post_id}
+                    id={post.post_id}
+                    picture={post.creatorInfo.picture}
+                    username={post.creatorInfo.username}
+                    text={post.caption}
+                    numberOfLikes={post.number_of_likes}
+                    postedAt={post.created_at}
+                    likes={post.likes}
+                    type='feed'
+                    setSelectedLikes={setSelectedLikes}
+                    // setSelectedLikes={setSelectedLikes}
+                  />
+                );
+              })}
+          </InfiniteScroll>
+        ) : null}
+      </div>
+      <div className='FEED_SIDE'>
+        <h3>Mypage</h3>
+        <hr></hr>
+      </div>
+      {selectedLikes && (
+        <LikesModal
+          closeHandler={setSelectedLikes}
+          info={selectedLikes}
+          title='Likes'
+        />
       )}
     </div>
   );
