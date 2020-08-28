a15a81ec88894dde6262e0042f4bb764db76c0cf
kostyniuk <kostyniuk7@gmail.com>
2020-08-28 19:58:40 +0300

    create a distinct folder

---
+++ client/src/components/User/Sides/InterestBar.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import '../../../styles/followingBar.css';
+import { useHistory } from 'react-router-dom';
+
+const InterestBar = () => {
+  const history = useHistory();
+
+  const currentPage = useSelector((state) => state.currentPage);
+  const loggedInUser = useSelector((state) => state.loggedInUser);
+
+  const { interests } = currentPage;
+
+  console.log({ interests, currentPage });
+
+  const redirectToSettings = () => {
+    history.push('/account');
+  };
+
+  if (!interests) return <div className='USER_INFO__followingBar'></div>;
+
+  return (
+    <div className='USER_INFO__followingBar'>
+      <div className='INTEREST_BAR_CONTAINER'>
+        <div className='INTEREST_BAR_TITLE'>
+          {interests.length || loggedInUser.id !== currentPage.id ? (
+            <h3>Interests</h3>
+          ) : null}
+        </div>
+        <div className='INTEREST_BAR_BODY'>
+          {interests.length ? (
+            <ul>
+              {interests.map((interest) => (
+                <li
+                  style={{
+                    backgroundColor: interest.interest_color,
+                    opacity: '.8',
+                    color: 'black',
+                  }}
+                >{`${interest.interest_emoji} ${interest.interest_name}`}</li>
+              ))}
+            </ul>
+          ) : null}
+          {!interests.length && loggedInUser.id === currentPage.id && (
+            <div className='INTEREST_BAR_NO_INTERESTS'>
+              <h3>No interests yet</h3>
+              <i
+                class='fa fa-plus'
+                aria-hidden='true'
+                onClick={redirectToSettings}
+              ></i>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InterestBar;
---
+++ client/src/components/User/Sides/PicturesBar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import { motion } from 'framer-motion';
+
+import '../../../styles/picturesBar.css';
+
+function PicturesBar({ setSelectedImg }) {
+  const pictures = [
+    { id: 1, url: 'https://as01.epimg.net/futbol/imagenes/2019/03/14/champions/1552569020_031070_1552569560_noticia_normal.jpg' },
+    { id: 2, url: '/api/public/uploads/user_dloading.jpg' },
+    { id: 3, url: '/api/public/uploads/N6NCsEnf_6U9RvrfYNXpb.jpg' },
+    { id: 4, url: '/api/public/uploads/user_tsunamipapi.jpg' },
+    { id: 5, url: '/api/public/uploads/user_dloading.jpg' },
+    { id: 6, url: '/api/public/uploads/N6NCsEnf_6U9RvrfYNXpb.jpg' },
+    { id: 7, url: '/api/public/uploads/user_tsunamipapi.jpg' },
+    { id: 8, url: 'https://as01.epimg.net/futbol/imagenes/2019/03/14/champions/1552569020_031070_1552569560_noticia_normal.jpg' },
+  ];
+
+  return (
+    <div className='USER_INFO__picturesBar'>
+      <div className='USER_INFO_picturesBar_header'>
+        <h3 className='USER_INFO__picturesBar__title'>My pictures &#8203; </h3>
+        <div className='USER_INFO__picturesBar__add'>
+          <div className='UPLOAD_btn_upload ADD_PICTURE'>
+            <input type='file' id='UPLOAD_PROFILE_PICTURE' name='' />
+            <i className='fa fa-plus' aria-hidden='true'></i>
+          </div>
+        </div>
+      </div>
+      <div className='img-grid'>
+        {pictures &&
+          pictures.map((doc) => (
+            <motion.div
+              className='img-wrap'
+              key={doc.id}
+              layout
+              onClick={() => setSelectedImg(doc.url)}
+            >
+              <motion.img
+                src={doc.url}
+                alt='uploaded pic'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1 }}
+              />
+            </motion.div>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default PicturesBar;
