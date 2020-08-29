958c344306e254ff594f895819988db4d807e3fb
kostyniuk <kostyniuk7@gmail.com>
2020-08-29 15:11:35 +0300

    make it much reusable

--- client/src/components/Header/AsyncSelect.js
+++ client/src/components/Header/AsyncSelect.js
@@ -1,13 +1,42 @@
 import React from 'react';
 
-
 import AsyncSelect from 'react-select/async';
 
 import useFetch from '../../hooks/useFetch';
 
-import {colorStyles} from './colorStylesSelect'  
+import { colorStyles } from './colorStylesSelect';
 
-const AsyncSelectCustom = ({ handler }) => {
+const addColorToUser = (users) =>
+  users.map((user) => ({ ...user, color: 'grey' }));
+
+const adjustCountry = (countries) =>
+  countries.map((country) => ({
+    value: country.code,
+    label: country.name,
+  }));
+const adjustCity = (cities) =>
+  cities.map((city) => ({
+    value: city.regionCode,
+    label: city.name,
+  }));
+
+const adjustToShowSelectValues = (data, type = '') => {
+  const lower = type.toLowerCase();
+
+  if (lower === 'country') return adjustCountry(data);
+  if (lower === 'city') return adjustCity(data);
+
+  return data;
+};
+
+const AsyncSelectCustom = ({
+  handler,
+  urlToFetch,
+  type,
+  noOptionsMessage = '',
+  requestParams = {},
+  isDisabled = false,
+}) => {
   const { request } = useFetch();
   let timeId = null;
 
@@ -17,22 +46,30 @@ const AsyncSelectCustom = ({ handler }) => {
         clearTimeout(timeId);
         timeId = setTimeout(async () => {
           const responce = await callback();
-          console.log({ responce });
-          resolve(responce.map((user) => ({ ...user, color: 'grey' })));
+          resolve(responce);
         }, timeout);
       });
-    return await debounce(
-      500,
-      request.bind(null, `/api/findUser/${inputValue}`)
-    );
+
+    const url = urlToFetch + inputValue;
+
+    let data = await debounce(500, request.bind(null, url, requestParams));
+
+    if (type.includes('RAPID')) {
+      data = adjustToShowSelectValues(data.data, type.split('RAPID_API_')[1]);
+    }
+
+    const coloredData = addColorToUser(data);
+
+    return coloredData;
   };
 
   return (
     <AsyncSelect
+      isDisabled={isDisabled}
       placeholder='Search...'
       cacheOptions
       styles={colorStyles}
-      noOptionsMessage={() => 'No such user'}
+      noOptionsMessage={() => noOptionsMessage}
       loadOptions={promiseOptions}
       onChange={handler}
     />
