2821190afb10c3cf40a79d27f9bc18117858cd90
kostyniuk <kostyniuk7@gmail.com>
2020-09-01 06:19:43 +0300

    implemented ResultCard

---
+++ client/src/components/Search/SearchResult.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ResultCard from './ResultCard';
+
+const SearchResult = () => {
+  return (
+    <div className='SEARCH_RESULTS'>
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+      <ResultCard />
+    </div>
+  );
+};
+
+export default SearchResult;
