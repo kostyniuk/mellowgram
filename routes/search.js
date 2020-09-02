245ebb5f63a87f11ae49b2d5b1fee534988d80d7
kostyniuk <kostyniuk7@gmail.com>
2020-09-02 11:26:54 +0300

    add obtaining a specific interest's information

--- routes/search.js
+++ routes/search.js
@@ -32,16 +32,24 @@ const isMatchAll = (bool, num) => {
   return '>= 1';
 };
 
-const prepareQuery = ({ interests, matchAll, country, city }) => {
-  const addParameters = formParams(interests.length, 0);
+const prepareQuery = ({
+  interests,
+  matchAll,
+  country,
+  city,
+  user_id,
+  limit = 9,
+  offset = 0,
+}) => {
+  const addParameters = formParams(interests.length, 3);
 
-  let query = `SELECT * FROM (SELECT user_info.user_id, user_info.username, pers.fullname, pers.occupation, pers.based_in, pers.picture, COUNT(uim.interest_id) as matched FROM User_info INNER JOIN Person pers ON pers.person_id = user_info.user_id INNER JOIN Users_interests_map uim ON uim.user_id = user_info.user_id INNER JOIN Interest int ON int.interest_id = uim.interest_id WHERE (int.interest_id IN (${addParameters}) AND pers.based_in LIKE '${based_inRegExp(
+  let query = `SELECT * FROM (SELECT * FROM (SELECT user_info.user_id, user_info.username, pers.fullname, pers.occupation, pers.based_in, pers.picture, COUNT(uim.interest_id) as matched FROM User_info INNER JOIN Person pers ON pers.person_id = user_info.user_id INNER JOIN Users_interests_map uim ON uim.user_id = user_info.user_id INNER JOIN Interest int ON int.interest_id = uim.interest_id WHERE (int.interest_id IN (${addParameters}) AND pers.based_in LIKE '${based_inRegExp(
     { country, city }
-  )}') GROUP BY user_info.user_id, pers.fullname, pers.occupation, pers.based_in, pers.picture ORDER BY COUNT(uim.interest_id) DESC LIMIT 6) AS Info_and_matched INNER JOIN users_interests_map ium ON ium.user_id = Info_and_matched.user_id WHERE Info_and_matched.matched ${isMatchAll(
+  )}' AND user_info.user_id != $1) GROUP BY user_info.user_id, pers.fullname, pers.occupation, pers.based_in, pers.picture ORDER BY COUNT(uim.interest_id) DESC LIMIT $2 OFFSET $3) AS Info_and_matched INNER JOIN users_interests_map ium ON ium.user_id = Info_and_matched.user_id WHERE Info_and_matched.matched ${isMatchAll(
     matchAll,
     interests.length
-  )} ORDER BY Info_and_matched.matched DESC;`;
-  let params = [...interests];
+  )} ORDER BY Info_and_matched.matched DESC) AS Matched_wo_Interests INNER JOIN Interest int ON int.interest_id = Matched_wo_Interests.interest_id`;
+  let params = [user_id, limit, offset, ...interests];
 
   console.log();
 
@@ -50,13 +58,21 @@ const prepareQuery = ({ interests, matchAll, country, city }) => {
 
 router.get('/', async (req, res, next) => {
   try {
+    const { user_id } = req.user;
+
     const { interests, matchAll, country, city } = req.query;
+
+    const limit = 9;
+    const offset = 0;
 
     const [query, params] = prepareQuery({
       interests: interests.split(','),
       matchAll,
       country,
       city,
+      user_id,
+      limit,
+      offset,
     });
 
     console.log({ query, params });
@@ -64,7 +80,18 @@ router.get('/', async (req, res, next) => {
     const { rows } = await db.query(query, params);
 
     console.log({ rows });
+    const prepareResponce = (arr) => {
+      const map = {};
 
+      arr.map((interestsInfo) => {
+        if (!map[interestsInfo.user_id])
+          map[interestsInfo.user_id] = { ...interestsInfo };
+      });
+
+      console.log({ map });
+    };
+
+    prepareResponce(rows);
     res.json({ interests: interests.split(','), matchAll, country, city });
   } catch (e) {
     console.log({ e });
