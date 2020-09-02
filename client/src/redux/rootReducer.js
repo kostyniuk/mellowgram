53f78777b4b9d73ed477986933dc29deeb103ea9
kostyniuk <kostyniuk7@gmail.com>
2020-09-02 20:31:26 +0300

    add searchHandler

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -11,6 +11,7 @@ import chatsReducer from './chatReducer';
 import messagesReducer from './messagesReducer';
 import onlineUsersReducer from './onlineUsers';
 import homePostsReducer from './homePostsReducer';
+import searchReducer from './searchReducer';
 
 const rootReducer = combineReducers({
   loggedInUser: loggedInUserReducer,
@@ -24,6 +25,7 @@ const rootReducer = combineReducers({
   messages: messagesReducer,
   onlineUsers: onlineUsersReducer,
   homePosts: homePostsReducer,
+  search: searchReducer,
 });
 
 export default rootReducer;
