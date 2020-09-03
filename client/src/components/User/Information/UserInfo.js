409f37a49e07216ca05d9ad59aa5f5aa2a49dfa8
kostyniuk <kostyniuk7@gmail.com>
2020-09-03 18:37:39 +0300

    update the card

--- client/src/components/User/Information/UserInfo.js
+++ client/src/components/User/Information/UserInfo.js
@@ -352,11 +352,13 @@ const UserInfo = ({ startMessagingHandler, setOpenDialog }) => {
                       <i className='fa fa-edit' onClick={handleEditClick}></i>
                     )}
                   </div>
-                  <p className='card-desc'>
-                    {info.bio || "The user hasn't provided bio information"}
-                  </p>
+                  <div className='card-desc'>
+                    <p>
+                      {info.bio || "The user hasn't provided bio information"}
+                    </p>
+                  </div>
                 </div>
-                <div className='card-social'>
+                {/* <div className='card-social'>
                   <a href='#'>
                     <svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
                       <path d='M15.997 3.985h2.191V.169C17.81.117 16.51 0 14.996 0c-3.159 0-5.323 1.987-5.323 5.639V9H6.187v4.266h3.486V24h4.274V13.267h3.345l.531-4.266h-3.877V6.062c.001-1.233.333-2.077 2.051-2.077z' />
@@ -385,7 +387,7 @@ const UserInfo = ({ startMessagingHandler, setOpenDialog }) => {
                       <path d='M23.994 24v-.001H24v-8.802c0-4.306-.927-7.623-5.961-7.623-2.42 0-4.044 1.328-4.707 2.587h-.07V7.976H8.489v16.023h4.97v-7.934c0-2.089.396-4.109 2.983-4.109 2.549 0 2.587 2.384 2.587 4.243V24zM.396 7.977h4.976V24H.396zM2.882 0C1.291 0 0 1.291 0 2.882s1.291 2.909 2.882 2.909 2.882-1.318 2.882-2.909A2.884 2.884 0 002.882 0z' />
                     </svg>
                   </a>
-                </div>
+                </div> */}
               </div>
               {/* <div className={sectionExperience} id='experience'>
                 <div className='card-content'>
