34de4e8cdca062068bf46fdf37386524ea182501
kostyniuk <kostyniuk7@gmail.com>
2020-09-06 18:52:09 +0300

    initial

--- sql/picture.sql
+++ sql/picture.sql
@@ -11,6 +11,8 @@ CREATE TABLE Picture
 ALTER TABLE Picture ADD CONSTRAINT pkPicture
   PRIMARY KEY (picture_id);
 
+ALTER TABLE Picture ALTER created_at SET DEFAULT NOW();
+
 ALTER TABLE Picture ADD CONSTRAINT fk_Picture_User_Id
   FOREIGN KEY (user_id) REFERENCES User_info (user_id)
   ON DELETE CASCADE;
