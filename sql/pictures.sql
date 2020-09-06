d1704f7fbb89eb63a09abf979e3ac54361493c6d
kostyniuk <kostyniuk7@gmail.com>
2020-09-06 17:34:32 +0300

    Picture sql queries

---
+++ sql/pictures.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Picture
+(
+
+  picture_id BiGSERIAL,
+  user_id INTEGER NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  path VARCHAR(100) NOT NULL
+
+);
+
+ALTER TABLE Picture ADD CONSTRAINT pkPicture
+  PRIMARY KEY (picture_id);
+
+ALTER TABLE Picture ADD CONSTRAINT fk_Picture_User_Id
+  FOREIGN KEY (user_id) REFERENCES User_info (user_id)
+  ON DELETE CASCADE;
